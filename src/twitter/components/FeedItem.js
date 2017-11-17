7d138ccfe9bff9cb1cbd74d60364fa66a37663f0
Olivier Nguyen <olivier_nguyen@live.fr>
2017-11-17 20:50:28 +0100

    style FeedItem component

--- src/twitter/components/FeedItem.js
+++ src/twitter/components/FeedItem.js
@@ -1,5 +1,6 @@
 // @flow
 import React, { Component } from 'react';
+import moment from 'moment';
 
 type Props = {
     data: {
@@ -21,19 +22,46 @@ export default class FeedItem extends Component<Props> {
         const { created_at, text, user } = this.props.data;
 
         const S = {
-            container: {},
+            container: {
+                display: 'flex',
+                position: 'relative',
+                padding: 15,
+                border: 'solid 1px #ededed'
+            },
             leftContainer: {},
             profile: {},
-            rightContainer: {},
-            screenName: {},
-            name: {},
-            text: {},
-            date: {},
+            rightContainer: {
+                marginLeft: 10,
+                display: 'flex',
+                flexDirection: 'column',
+            },
+            screenName: {
+                color: '#bdbdbd',
+                fontSize: 13,
+            },
+            name: {
+                marginBottom: 5,
+            },
+            text: {
+                fontSize: 14,
+                color: '#6b6b6b',
+            },
+            date: {
+                position: 'absolute',
+                right: 0,
+                top: 0,
+                fontSize: 12,
+                padding: 5,
+            },
         };
+
+        const dateTweet = moment
+            .duration(moment().diff(moment(created_at)))
+            .humanize();
 
         return (
             <div style={S.container}>
-                <div style={S.date}>{created_at}</div>
+                <div style={S.date}>{dateTweet}</div>
                 <div style={S.leftContainer}>
                     <img
                         style={S.profile}
@@ -42,7 +70,7 @@ export default class FeedItem extends Component<Props> {
                     />
                 </div>
                 <div style={S.rightContainer}>
-                    <div style={S.screenName}>{user.screen_name}</div>
+                    <div style={S.screenName}>{`@${user.screen_name}`}</div>
                     <div style={S.name}>{user.name}</div>
                     <div style={S.text}>{text}</div>
                 </div>
