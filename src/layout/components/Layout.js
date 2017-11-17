10be568682bee9fd0b85ebca1dac44d6752f1d46
Olivier Nguyen <olivier_nguyen@live.fr>
2017-11-17 20:12:21 +0100

    insert mock data + declare flow types

--- src/layout/components/Layout.js
+++ src/layout/components/Layout.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
+import FeedList from '../../twitter/components/FeedList';
+import { extractTwitterStatuses } from '../../utils/utils';
+import { MOCK_TWITTER_API_SEARCH_REACTJS } from '../../mock';
 
 export default class Layout extends Component {
+    static defaultProps = {
+        data: extractTwitterStatuses(MOCK_TWITTER_API_SEARCH_REACTJS)
+    };
+
     render() {
-        return <div>Layout</div>;
+        return (
+            <div>
+                <FeedList data={this.props.data}/>
+            </div>
+        );
     }
 }
--- src/twitter/components/FeedList.js
+++ src/twitter/components/FeedList.js
@@ -1,6 +1,22 @@
+// @flow
 import React, { Component } from 'react';
 
-export default class FeedList extends Component {
+type Props = {
+    data: Array<{
+        id: number,
+        created_at: string,
+        text: string,
+        user: {
+            id: number,
+            name: string,
+            screen_name: string,
+            description: string,
+            profile_image_url: string,
+        },
+    }>,
+};
+
+export default class FeedList extends Component<Props> {
     render() {
         return <div>FeedList</div>;
     }
